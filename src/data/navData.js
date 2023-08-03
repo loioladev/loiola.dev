b1babd1d2cf43438b8e6467dc1f27d5d0664989e
Matheus Loiola <matheusloiolapinto@gmail.com>
2023-08-03 18:27:56 -0300

    wip: improving website

---
+++ src/data/navData.js
@@ -0,0 +1,20 @@
+const navData = [
+    {
+        name: "About",
+        path: "/about/"
+    },
+    {
+        name: "Projects",
+        path: "/projects/"
+    },
+    {
+        name: "CSES",
+        path: "/cses/"
+    },
+    {
+        name: "Blog",
+        path: "/blog/"
+    }
+]
+
+export default navData;
